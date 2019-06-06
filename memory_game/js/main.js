06e31fc31d47e4269d1be076ee1f7e71f9158054
Marcus <marcusteo@hotmail.com>
2019-06-06 23:48:59 +0800

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,19 @@
 console.log ("Up and running!")
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[3];
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+console.log ("User flipped queen");
+cardsInPlay.push(cardTwo);
+console.log ("User flipped king");
+
+if (cardsInPlay.length === 2){
+  console.log ("all good");
+  if (cardsInPlay[0]===cardsInPlay[1]){
+    alert ("Well done!")
+  }
+  else {
+    alert ("Oops! Try again!")
+  }
+}
