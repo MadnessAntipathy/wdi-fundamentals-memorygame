56d2d6b5728bac407c1b990c8414c88a6b18a0b2
Marcus <marcusteo@hotmail.com>
2019-06-08 17:06:11 +0800

    created object for cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,50 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+},
+];
+
+//array to hold cards in play
 var cardsInPlay = [];
 
+//check for match of two selected cards
 function checkForMatch (){
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0]===cardsInPlay[1]){
+      alert("You found a match!");
       console.log("You found a match!");
     }
     else {
+      alert("Oops, try again!");
       console.log("Oops, try again!");
     }
   }
 }
 
+//when user flips card
 function flipCard (cardId){
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank)
+  console.log (cards[cardId].cardImage);
+  console.log (cards[cardId].suit);
   checkForMatch();
 }
+flipCard (0);
+flipCard (2);
