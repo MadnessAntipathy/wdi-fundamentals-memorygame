972992a92a5f0c9ffa496284f7d343c493b675f1
Marcus <marcusteo@hotmail.com>
2019-06-14 15:51:16 +0800

    housekeeping

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,25 +26,43 @@ var cardsInPlay = [];
 
 //check for match of two selected cards
 function checkForMatch (){
-  if (cardsInPlay.length === 2) {
     if (cardsInPlay[0]===cardsInPlay[1]){
       alert("You found a match!");
+      resetGame();
       console.log("You found a match!");
     }
     else {
       alert("Oops, try again!");
+      resetGame();
       console.log("Oops, try again!");
     }
-  }
 }
 
 //when user flips card
-function flipCard (cardId){
+function flipCard (){
+  var cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank)
   console.log (cards[cardId].cardImage);
   console.log (cards[cardId].suit);
-  checkForMatch();
+  this.setAttribute("src", cards[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
 }
-flipCard (0);
-flipCard (2);
+
+function createBoard(){
+  for (var i = 0; i <cards.length; i++){
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+
+function resetGame(){
+  location.reload();
+}
+
+createBoard();
